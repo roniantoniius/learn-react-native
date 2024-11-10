b8d36af21d1223580ef7ed87cfe51b74d50d002f
roniantonius <roniantoniuss@upi.edu>
2024-11-10 22:23:04 +0700

    just minor revision

--- markdown/jejak-laut/src/Note.tsx
+++ markdown/jejak-laut/src/Note.tsx
@@ -2,6 +2,7 @@ import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
 import { useNote } from "./NoteLayout";
 import { Link, useNavigate } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
+import styles from "./NoteList.module.css";
 
 type NoteProps = {
   onDelete: (id: string) => void;
@@ -17,10 +18,9 @@ export function Note({ onDelete }: NoteProps) {
         <Col>
           <h1>{note.title}</h1>
           {note.tags.length > 0 && (
-            <Stack gap={1} direction="horizontal" className=" flex-wrap">
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
               {note.tags.map((tag) => (
                 <Badge key={tag.id} className="text-truncate">
-                  {" "}
                   {tag.label}
                 </Badge>
               ))}
@@ -47,7 +47,14 @@ export function Note({ onDelete }: NoteProps) {
           </Stack>
         </Col>
       </Row>
-      <ReactMarkdown>{note.markdown}</ReactMarkdown>
+
+      {/* Garis pembatas horizontal */}
+      <hr className={styles.horizontalDivider} />
+
+      {/* Kontainer untuk Markdown dengan jarak di bagian atas */}
+      <div className={`${styles.markdownContainer} mt-3`}>
+        <ReactMarkdown>{note.markdown}</ReactMarkdown>
+      </div>
     </>
   );
 }
